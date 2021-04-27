e610a760bfa8ad042b8d44df8785012c8bf8f564
Sam Coppini <samcoppini@gmail.com>
2021-04-26 21:30:25 -0700

    Generate stack jump

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -74,12 +74,70 @@ XrfContext getGenerationContext(llvm::Module &module, llvm::LLVMContext &context
     return xrfContext;
 }
 
+llvm::BasicBlock *createStackJump(llvm::LLVMContext &context, XrfContext &xrfContext, const std::vector<llvm::BasicBlock*> chunks) {
+    auto stackJump = llvm::BasicBlock::Create(context, "stack-jump", xrfContext.mainFunc);
+
+    auto errorBlock = llvm::BasicBlock::Create(context, "stack-error", xrfContext.mainFunc);
+
+    llvm::IRBuilder errorBuilder(errorBlock);
+
+    errorBuilder.CreateUnreachable();
+
+    llvm::IRBuilder builder(stackJump);
+
+    auto topValue = builder.CreateLoad(
+        llvm::IntegerType::getInt32Ty(context),
+        xrfContext.topValue
+    );
+
+    auto switchInst = builder.CreateSwitch(topValue, errorBlock, chunks.size());
+
+    for (size_t i = 0; i < chunks.size(); i++) {
+        switchInst->addCase(
+            llvm::ConstantInt::get(llvm::IntegerType::getInt32Ty(context), i),
+            chunks[i]
+        );
+    }
+
+    return stackJump;
+}
+
+void generateCodeForChunk(llvm::LLVMContext &context, XrfContext &xrfContext, const Chunk &chunk,
+                          llvm::BasicBlock *chunkBlock, llvm::BasicBlock *stackJump)
+{
+    llvm::IRBuilder builder(chunkBlock);
+
+    builder.CreateBr(stackJump);
+}
+
+void generateChunks(llvm::LLVMContext &context, XrfContext &xrfContext, const std::vector<Chunk> &chunks) {
+    std::vector<llvm::BasicBlock*> chunkStarts;
+
+    for (size_t i = 0; i < chunks.size(); i++) {
+        chunkStarts.push_back(
+            llvm::BasicBlock::Create(context, "chunk" + std::to_string(i), xrfContext.mainFunc)
+        );
+    }
+
+    auto *stackJump = createStackJump(context, xrfContext, chunkStarts);
+
+    for (size_t i = 0; i < chunks.size(); i++) {
+        generateCodeForChunk(context, xrfContext, chunks[i], chunkStarts[i], stackJump);
+    }
+
+    llvm::IRBuilder builder(xrfContext.startBlock);
+
+    builder.CreateBr(chunkStarts[0]);
+}
+
 } // anonymous namespace
 
 std::unique_ptr<llvm::Module> generateCode(llvm::LLVMContext &context, const std::vector<Chunk> &chunks) {
     auto module = std::make_unique<llvm::Module>("xrf", context);
 
     auto xrfContext = getGenerationContext(*module, context);
+
+    generateChunks(context, xrfContext, chunks);
 
     return module;
 }
